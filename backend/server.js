f66b973da6bc3f67e6c7a1dd1b4fb00b117e58f1
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-04-26 14:24:59 +0500

    register controller and login controller

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./src/config/db");
+const authRoutes = require("./src/routes/authRoutes");
 dotenv.config();
 connectDB();
 
@@ -11,6 +12,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

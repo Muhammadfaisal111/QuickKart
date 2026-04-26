318b9b02810680ba2579b613a4de963d9ba94f10
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-04-26 14:29:16 +0500

    send email function setup in utils

---
+++ backend/src/utils/sendEmail.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async ({ email, subject, message }) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    });
+
+    const mailOptions = {
+      from: "QuickCart Support" `<${process.env.EMAIL_USER}>`,
+      to: email,
+      subject,
+      html: message
+    };
+    await transporter.sendMail(mailOptions);
+    console.log(`Email sent to ${email}`);
+  } catch (error) {
+    console.error(`Error sending email to ${email}:`, error);
+  }
+};
+
+module.exports = sendEmail;

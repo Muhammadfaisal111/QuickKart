579de4d58eca9efd79e326eba85560db725ae378
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-04-26 12:37:44 +0500

    database connection and server start and folder structure

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./src/config/db");
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
